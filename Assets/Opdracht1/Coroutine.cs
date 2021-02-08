82124a2338c49e98297fac357933f8fe051c2ee9
thomasberrens <48439074+thomasberrens@users.noreply.github.com>
2021-02-08 12:11:32 +0100

    opdracht1

---
+++ Assets/Opdracht1/Coroutine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f")) 
+        {
+            StartCoroutine(Check());
+        }
+    }
+
+    IEnumerator Check()
+    {
+        Debug.Log("Ik start nu de coroutine");
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("coroutine update");
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("coroutine einde");
+    }
+}
